69b7f42fd2751b63d39fe27fadba0c26ef2fb208
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-04-28 15:01:07 -0700

    initialized

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+def print_function():
+    print("hello")
+
+print_function()
+
+
+def multiply_by_10(n):
+    n *= 10
+    num = n
+    return n
+
+
+# format() method
+# %%
+string1 = "Hello {person}".format(person = "Scoobert")
+print(string1)
+
+
+# %%
