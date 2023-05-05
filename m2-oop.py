4053a57bc04996bc1aab48582df8f3b5cb0702b0
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-05-05 15:30:28 -0700

    poly practice

--- m2-oop.py
+++ m2-oop.py
@@ -247,3 +247,35 @@ class XShape(Shape):
 circle = XShape("Circle")
 print(circle.getName())
 # %%
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+    
+    def Animal_details(self):
+        print(self.name + ", " + self.sound)
+    
+class Dog(Animal):
+    def __init__(self, name, sound, family):
+        super().__init__(name, sound)
+        self.family = family
+    
+    def Animal_details(self):
+        super().Animal_details()
+        print(self.family)
+
+class Sheep(Animal):
+    def __init__(self, name, sound, color):
+        super().__init__(name, sound)
+        self.color = color
+
+    def Animal_details(self):
+        super().Animal_details()
+        print(self.color)
+
+d = Dog("Pongo", "Woof Woof", "Husky")
+d.Animal_details()
+print("")
+s = Sheep("Billy", "Baa Baa", "White")
+s.Animal_details()
+# %%
