b8f22c0e4c395e0313fb11646f82ce3583ac0296
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-05-25 13:55:47 -0700

    stacks

--- m3-dataStructures_and_algorithms.py
+++ m3-dataStructures_and_algorithms.py
@@ -186,3 +186,116 @@ class Stack():
     
     def get_stack(self):
         return self.items
+    
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    else:
+        return False
+    
+def is_paren_balanced(paren_string):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+        if paren in "([{":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+                break
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+                    break
+        index += 1
+
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+    
+print(is_paren_balanced("(((({}))))"))
+print(is_paren_balanced("(((({})))"))
+# %%
+# Reversing a string in a stack
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def get_stack(self):
+        return self.items
+    
+def reverse_string(stack, input_str):
+    for i in range(len(input_str)):
+        stack.push(input_str[i])
+    rev_str = ""
+    while not stack.is_empty():
+        rev_str += stack.pop()
+    
+    return rev_str
+
+stack = Stack()
+input_str = "!evitacudE ot emocleW"
+print(reverse_string(stack, input_str))
+# %%
+# Exercise: Convert Decimal Integer to Binary
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def get_stack(self):
+        return self.items
+    
+def convert_int_to_bin(dec_num):
+    if dec_num == 0:
+        return 0
+    
+    s = Stack()
+
+    while dec_num > 0:
+        remainder = dec_num % 2
+        s.push(remainder)
+        dec_num = dec_num // 2
+    
+    bin_num = ""
+    while not s.is_empty():
+        bin_num += str(s.pop())
+    
+    return bin_num
+
+print(convert_int_to_bin(56))
+# %%
