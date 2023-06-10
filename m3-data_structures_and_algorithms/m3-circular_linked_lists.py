2e91355f925a409f7d932f2b6ede08221bc7e3fe
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-06-09 19:53:28 -0700

    circular lists done

--- m3-data_structures_and_algorithms/m3-circular_linked_lists.py
+++ m3-data_structures_and_algorithms/m3-circular_linked_lists.py
@@ -140,3 +140,16 @@ class CircularLinkedList:
             length -= 1
             
 
+# %%
+# Exercise: Is Circular Linked List
+def is_circular_linked_list(self, input_list):
+    if input_list.head:
+        cur = input_list.head
+        while cur.next:
+            cur = cur.next
+            if cur.next == input_list.head:
+                return True
+        return False
+    else:
+        return False
+#
