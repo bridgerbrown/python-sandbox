4af63aecdfd98a37fa4a7f537dff8dfc184baa74
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-01 14:40:58 -0700

    Course completed

--- m4-advanced-concepts/m4-adv-concepts-2.py
+++ m4-advanced-concepts/m4-adv-concepts-2.py
@@ -591,3 +591,74 @@ if __name__ == '__main__':
         my_thread = threading.Thread(
             target=update_total, args=(5,))
         my_thread.start()
+
+
+#__The multiprocessing Module__
+import os
+
+from multiprocessing import Process
+
+
+def doubler(number):
+    result = number * 2
+    proc = os.getpid()
+    print('{0} doubled to {1} by process id: {2}'.format(
+        number, result, proc))
+
+if __name__ == '__main__':
+    numbers = [5, 10, 15, 20, 25]
+    procs = []
+
+    for index, number in enumerate(numbers):
+        proc = Process(target=doubler, args=(number,))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+
+
+#__The concurrent.futures Module__
+import os
+import urllib.request
+
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+
+
+def downloader(url):
+    """
+    Downloads the specified URL and saves it to disk
+    """
+    req = urllib.request.urlopen(url)
+    filename = os.path.basename(url)
+    ext = os.path.splitext(url)[1]
+    if not ext:
+        raise RuntimeError('URL does not contain an extension')
+
+    with open(filename, 'wb') as file_handle:
+        while True:
+            chunk = req.read(1024)
+            if not chunk:
+                break
+            file_handle.write(chunk)
+    msg = 'Finished downloading {filename}'.format(filename=filename)
+    return msg
+
+
+def main(urls):
+    """
+    Create a thread pool and download specified urls
+    """
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(downloader, url) for url in urls]
+        for future in as_completed(futures):
+            print(future.result())
+
+if __name__ == '__main__':
+    urls = ["http://www.irs.gov/pub/irs-pdf/f1040.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040a.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040ez.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040es.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040sb.pdf"]
+    main(urls)
